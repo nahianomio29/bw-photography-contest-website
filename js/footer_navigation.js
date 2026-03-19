8be2131952bc664bc31c9e8c3e5769e3722f9559
Nahian Omio <nahianomio543@gmail.com>
2026-03-19 08:46:24 -0400

    Add files via upload

---
+++ js/footer_navigation.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+/* This is an example of footer navigational text hyperlinks */
+/* Use the script tag to reference this external file from within your HTML document in the head section */
+
+document.write("<div style='text-align:center;'>");
+document.write("<a href='https://omion043.macombserver.net/itwp1000/project3/index.html'>Home</a> | <a href='https://omion043.macombserver.net/itwp1000/project3/rules.html'>Rules and Regulations</a> | <a href='https://omion043.macombserver.net/itwp1000/project3/winners.html'>2023 Winners</a> | <a href='https://omion043.macombserver.net/itwp1000/project3/voting.html'>Voting</a> | <a href='https://omion043.macombserver.net/itwp1000/project3/submit.html'>Submit Your Photography</a> | <a href='https://omion043.macombserver.net/itwp1000/project3/contact.html'>Contact Us</a> | <a href='https://omion043.macombserver.net/itwp1000/project3/sitemap.xml'>XML Sitemap</a> | <a href='https://omion043.macombserver.net/itwp1000/home.htm'>Course Homepage</a>");
+document.write("</div>");
